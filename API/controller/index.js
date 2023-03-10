7b54afecf8b63d2d50eafada0cbdd1daced485a4
giftnkz6 <111094771+giftnkz6@users.noreply.github.com>
2023-03-10 11:01:47 +0200

    added post method

--- API/controller/index.js
+++ API/controller/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 // path
 const path = require('path');
 // body-parser
-const bodyParser = require('body-parser');
+const parser = require('body-parser');
 // Router
 const route = express.Router();
 // Models

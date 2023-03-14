a5f53f55a79281cb5de2427e5010045da0b9baf9
giftnkz6 <111094771+giftnkz6@users.noreply.github.com>
2023-03-14 16:55:14 +0200

    Shop

--- gift_clothing_hub/src/views/ShopView.vue
+++ gift_clothing_hub/src/views/ShopView.vue
@@ -1,15 +1,28 @@
 <template>
-    <div>
-        <ShopVue/>
+    <h1>Hello</h1>
+    <div v-for="product in products" :key="product.id">
+        <div class="card" style="width: 18rem;">
+            <img :src="product.imgURL" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">{{product.prodName}}</h5>
+                <p class="card-text">{{ product.prodDescription }}</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-import ShopVue from '@/components/ShopVue.vue';
 export default {
     name: "ShopView",
-    components:{
-        ShopVue
+    computed: {
+        products: function () {
+            return this.$store.state.products
+        }
+    },
+
+    computed() {
+        this.$store.dispatch("getProducts")
     }
 }
 </script>

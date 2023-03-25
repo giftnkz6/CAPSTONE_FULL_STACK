a3122afd8e4d9ab86a49f72ef3e4d54cb4a81888
giftnkz6 <111094771+giftnkz6@users.noreply.github.com>
2023-03-25 13:44:58 +0200

    API

---
+++ config/index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const {createPool} = require('mysql2');
+// Create connection variable
+let connection = createPool({
+  host: process.env.HOST,
+  user: process.env.USERNAME,
+  password: process.env.USER_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.PORT_DB,
+  multipleStatements: true,
+  connectionLimit: 30
+});
+
+
+module.exports = connection;
